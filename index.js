b30ac074f51278f5541a687afef58833a373d331
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-02-27 16:47:08 +0700

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
+app.listen(PORT, () => console.log(`Server listen at port ${PORT}`));
