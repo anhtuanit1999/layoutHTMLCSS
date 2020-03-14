005590c4e19d45d4c8a11691cc118552bfc6b95d
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-14 09:26:32 +0700

    them layout flexbox

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/bai1', (req, res) => res.render('bai1'));
+app.get('/bai2', (req, res) => res.render('bai2'));
+app.get('/bai3', (req, res) => res.render('bai3'));
 app.listen(PORT, () => console.log(`Server listen at port ${PORT}`));
